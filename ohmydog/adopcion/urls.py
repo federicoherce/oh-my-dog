f33afe505194a8b59763203c6cf5419ca0b4ff7f
Gian <gianq7@gmail.com>
2023-05-21 00:20:03 -0300

    casi finalizados el listar adopcion y modificar

--- ohmydog/adopcion/urls.py
+++ ohmydog/adopcion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import publicar, listado
+from .views import publicar, listar_perros_adopcion, marcar_Adoptado, eliminar_perro_en_adopcion
 
 urlpatterns = [
-    path('', listado, name="perros_en_adopcion"),
+    path('perros_en_adopcion', listar_perros_adopcion, name="perros_en_adopcion"),
     path('publicar/', publicar, name='publicar_perro'),
+    path('cliente/<int:perro_id>/marcar_Adoptado/', marcar_Adoptado, name='marcar_Adoptado'),
+    path('cliente/<int:perro_id>/eliminar/', eliminar_perro_en_adopcion, name='eliminar_perro_en_adopcion')
 ]
--- ohmydog/adopcion/views.py
+++ ohmydog/adopcion/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import PublicarPerroEnAdopcion
+from .models import Adopcion
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -26,3 +27,32 @@ def publicar(request):
 
 def listado(request):
     return render(request, "perros_en_adopcion.html")
+
+def listar_perros_adopcion(request):
+    perros_en_adopcion = Adopcion.objects.all
+
+    if request.method == "POST":
+        #perro_a_borrar = Adopcion.objects.get(nombre=request.POST['perros_adopcion.'])
+        #perro_a_borrar.delete()
+        redirect("perros_en_adopcion")
+
+    return render(request, "perros_en_adopcion.html", {
+        'perros_en_adopcion': perros_en_adopcion
+    })
+
+def marcar_Adoptado(request, perro_id):
+    perroAdoptado = Adopcion.objects.get(id=perro_id)
+
+    if request.method == 'POST':
+        perroAdoptado.adoptado = not perroAdoptado.adoptado
+        perroAdoptado.save()
+
+    return redirect('perros_en_adopcion')
+
+def eliminar_perro_en_adopcion(request, perro_id):
+    perro = Adopcion.objects.get(id=perro_id)
+
+    if request.method == 'POST':
+        perro.delete()
+    
+    return redirect('perros_en_adopcion')
