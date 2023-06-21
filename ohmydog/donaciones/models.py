901f3579f0d58ee980e1564773989dc1a49c4305
Gian <gianq7@gmail.com>
2023-06-21 11:50:52 -0300

    se arregla boton volver y donacion mayor a 1

--- ohmydog/donaciones/models.py
+++ ohmydog/donaciones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from autenticacion.models import CustomUser
@@ -8,7 +9,7 @@ from autenticacion.models import CustomUser
 class Campaña(models.Model):
     nombre = models.CharField(max_length=30, unique=True, error_messages={
             'unique': 'Ya existe un usuario con este email'})
-    monto_objetivo = models.DecimalField(decimal_places=2, max_digits=12)
+    monto_objetivo = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(1)])
     descripcion = models.TextField(max_length=200)
     finalizada = models.BooleanField(default=False)
 
@@ -18,7 +19,7 @@ class Donacion(models.Model):
         ('Veterinaria', 'Veterinaria'),
     ]
 
-    monto = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    monto = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
     nombre = models.CharField(max_length=30, blank=True, default="")
     fecha = models.DateField(default=timezone.now)
     tipo = models.CharField(max_length=12, choices=TIPO_CHOICES)
